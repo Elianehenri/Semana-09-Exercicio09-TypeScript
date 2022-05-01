3a45b16bcbf1bb110d685e1962a210d1441b44d9
elianehenri <elianehenriqueta@gmail.com>
2022-05-01 15:25:08 -0300

    Exercicio 09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../assets/exercicio09';
 
 @Component({
   selector: 'ntf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'exercicio09';
+export class AppComponent implements OnInit, Produto{
+
+  valorTotal: number = 0;
+  nome: string = "";
+  valor: number = 0;
+  codigo: number = 0;
+  quantidade: number = 0;
+  emEstoque: boolean = true;
+  sofa:Produto = { 
+    nome: "Sofá 4 lugares",
+    valor: 120,
+    codigo: 1,
+    quantidade: 30,
+    emEstoque: true,
+  };
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+    this.comprar(this.sofa, 30);
+    this.comprar(this.sofa, 10);
+
+  }
+
+  comprar(produto: Produto, quantidade: number): void {
+
+    if (produto.emEstoque) {
+      this.valorTotal = produto.valor * quantidade;
+      debugger;
+      produto.quantidade = produto.quantidade - quantidade;
+    }
+
+  }
 }
